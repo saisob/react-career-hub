044eb02fb4c79c8e677f2ce69761d3eddc3263fa
saisob <saisob32@gmail.com>
2024-04-04 20:10:21 +0600

    use icon and added see all jobs button

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -2,21 +2,35 @@ import { useEffect, useState } from "react";
 import Job from "../Job/Job";
 
 const Featured = () => {
-    const[jobs,setJobs]=useState([]);
 
-    useEffect(()=>{
+    const [dataLength, setDataLength] = useState([4])
+
+    const [jobs, setJobs] = useState([]);
+
+    useEffect(() => {
         fetch("jobs.json")
-        .then(res => res.json())
-        .then(data =>setJobs(data))
+            .then(res => res.json())
+            .then(data => setJobs(data))
     }, [])
     return (
-        <div className="text-center space-y-4 my-10">
-            <h1 className="text-5xl font-bold">Featured Jobs:{jobs.length}</h1>
-            <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
-            <div className="grid grid-cols-2 gap-6">
-                {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
-                }
+        <div className="my-10">
+            <div className="text-center space-y-4 ">
+                <h1 className="text-5xl font-bold">Featured Jobs:{jobs.length}</h1>
+                <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+                <div className="grid grid-cols-2 gap-6">
+                    {
+                        jobs.slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)
+                    }
+                </div>
+
+            </div>
+            <div className={dataLength === jobs.length && 'hidden'}>
+                <div className="text-center mt-10">
+                    <button className="btn btn-primary"
+                        onClick={() => setDataLength(jobs.length)}
+                    >See All jobs</button>
+                </div>
+
             </div>
         </div>
     );
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,20 +1,36 @@
 
+import { MdLocationOn } from 'react-icons/md';
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
+
 const Job = ({ job }) => {
-    const { logo,job_title,company_name,remote_or_onsite,location,job_type,salary,address  } = job;
+    const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary, address } = job;
     return (
-        <div className="card card-compact pt-5 bg-gray-100 shadow-xl ">
-            <figure><img src={logo} alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title mx-auto">{job_title}</h2>
-                <p>{company_name}</p>
-                <div className="flex space-x-4">
-                    <button className="btn btn-outline">{remote_or_onsite}</button>
-                    <button className="btn btn-outline">{job_type}</button>
-                </div>
-                <div className="card-actions">
-                    <button className="btn btn-primary">View Details</button>
+        <div>
+            <div className="card card-compact pt-5 bg-gray-100 shadow-xl ">
+                <figure><img src={logo} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title mx-auto">{job_title}</h2>
+                    <p>{company_name}</p>
+                    <div className="flex space-x-4">
+                        <button className="btn btn-outline">{remote_or_onsite}</button>
+                        <button className="btn btn-outline">{job_type}</button>
+                    </div>
+                    <div className='flex justify-between '>
+                        <div className='flex'>
+                            <h1 className='text-2xl'><MdLocationOn></MdLocationOn></h1> {location}
+                        </div>
+                        <div className='flex space-x-2'>
+                            <h1 className='text-2xl'><RiMoneyDollarCircleLine></RiMoneyDollarCircleLine></h1>
+                            <p>salary:{salary}</p>
+                        </div>
+                    </div>
+
+                    <div className="card-actions">
+                        <button className="btn btn-primary">View Details</button>
+                    </div>
                 </div>
             </div>
+          
         </div>
     );
 };
